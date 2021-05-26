23e3605bda2b155e8c6bff1e2cf324f818f82e80
johnnyapu <juahn.jeong@lgcns.com>
2021-05-26 16:26:23 +0900

    add socketio event

--- src/middleware/io/index.ts
+++ src/middleware/io/index.ts
@@ -1,20 +1,41 @@
+/* eslint-disable no-multi-spaces */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-shadow */
 export * as socket from './init';
 
 export enum SocketEvent {
-    INIT = 'INIT',
-    ATTACH = 'ATTACH',
-    DETACH = 'DETACH',
-    SIGNAL = 'SIGNAL',
-    PRESIGNAL = 'PRESIGNAL',
+    INIT            = 'INIT',
+    ATTACH          = 'ATTACH',
+    DETACH          = 'DETACH',
+    SIGNAL          = 'SIGNAL',
+    PRESIGNAL       = 'PRESIGNAL',
+
+    CREATE_NOTE     = 'CREATE_NOTE',
+    UPDATE_NOTE     = 'UPDATE_NOTE',
+    DELETE_NOTE     = 'DELETE_NOTE',
+    RETRIEVE_NOTE   = 'RETRIEVE_NOTE',
+    REFRESH_NOTE    = 'REFRESH_NOTE',
+
+    RESPONSE        = 'RESPONSE',
 }
 
-export type SocketMessage = {
-    socketEvent: SocketEvent
-    markerId: string
-    members?: string[]
-    sender: string
-    data?: string
-    receiver?: string
+export type WebRTCMessage = {
+    socketEvent     : SocketEvent
+    markerId        : string
+    members?        : string[]
+    sender          : string
+    data?           : string
+    receiver?       : string
+}
+
+export type NoteMessage = {
+    socketEvent     : SocketEvent
+    markerId        : string
+    userId          : string
+    noteId          : string
+    noteText        : string
+    posX            : number
+    posY            : number
+    createDt        : string
+    updateDt        : string
 }
