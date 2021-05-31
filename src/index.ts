a25f66fd2be4a2b59a2519bcce7a754b4bbb71c1
johnnyapu <juahn.jeong@lgcns.com>
2021-05-31 15:05:40 +0900

    static undo

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,11 @@ import { socket } from './middleware/io';
 import router from './router/index';
 
 const app = express();
-app.use('/public', express.static('public'));
 const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
-const httpPort = process.env.HTTP_PORT ? parseInt(process.env.HTTP_PORT, 10) : 3002;
 const server = new httpServer.Server(app);
 socket.initWS(server);
 
 app.use('/', router);
-app.listen(httpPort, '0.0.0.0')
 server.listen(
   port, '0.0.0.0', undefined,
   () => {
