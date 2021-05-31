9e05962b9aec413cb5fb3a3a89936d444d255be4
johnnyapu <juahn.jeong@lgcns.com>
2021-05-31 14:51:42 +0900

    public 위치 이동

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { socket } from './middleware/io';
 import router from './router/index';
 
 const app = express();
-app.use(express.static('public'));
+app.use('/public', express.static('public'));
 let port = process.env.PORT ?? 3000;
 if (typeof (port) === 'string') {
   port = parseInt(port, 10);
@@ -13,7 +13,7 @@ const server = new httpServer.Server(app);
 socket.initWS(server);
 
 app.use('/', router);
-
+app.listen(3001, '0.0.0.0')
 server.listen(
   port, '0.0.0.0', undefined,
   () => {
