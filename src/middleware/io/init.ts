a20a39747199e212400beea55a08a15021188983
Jeong Juahn <johnnyapu15@gmail.com>
2021-06-10 00:46:24 +0900

    broadcast emit 수정

--- src/middleware/io/init.ts
+++ src/middleware/io/init.ts
@@ -29,7 +29,7 @@ export async function broadcast(metadata: SocketMetadata, msg: WebRTCMessage | N
   // 이 서버에 연결된 소켓에 해당하는 멤버에게 브로드캐스트
   console.log(`broadcast ${JSON.stringify(metadata.namespace.sockets)}`)
   if (msg.socketEvent) {
-    metadata.namespace
+    metadata.namespace.adapter
       .emit(msg.socketEvent, msg);
   }
 }
