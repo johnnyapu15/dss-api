8815d4dab85183c80d5d68eb3a952fb17d4c8825
johnnyapu15 <johnnyapu15@gmail.com>
2021-06-23 17:55:53 +0900

    id 관리 버그 픽스

--- src/middleware/io/commonFunctions.ts
+++ src/middleware/io/commonFunctions.ts
@@ -22,7 +22,7 @@ export function getMemberAddr(msg: WebRTCMessage) {
  * @param id if null, use random uuid.
  * @returns created id
  */
-export async function allocID(markerId: string, id?: string) {
+export async function allocID(id?: string) {
   // eslint-disable-next-line no-param-reassign
   if (!id) { id = generateUUID(); }
 
